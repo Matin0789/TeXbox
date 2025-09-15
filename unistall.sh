8851664a193b63abb8366a415b39ed1d5de87631
matin0789 <matin.a0789@gmail.com>
2025-09-15 14:49:29 +0330

    Update uninstall.sh for zsh
    Signed-off-by: matin0789 <matin.a0789@gmail.com>

--- unistall.sh
+++ unistall.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+
 INSTALL_DIR="$HOME/.texbox"
+
 rm -rf "$INSTALL_DIR/bin/texbox"
-if [ -f ~/.bashrc ]; then
-    sed -i '/export PATH="$HOME\/.texbox\/bin:\$PATH"/d' ~/.bashrc
+
+if [ -n "$ZSH_VERSION" ]; then
+    SHELL_RC="$HOME/.zshrc"
+elif [ -n "$BASH_VERSION" ]; then
+    SHELL_RC="$HOME/.bashrc"
+else
+    echo "Unsupported shell. Remove $INSTALL_DIR/bin from PATH manually."
+    exit 1
 fi
-echo "TeXbox uninstalled. Restart terminal or run 'source ~/.bashrc'"
+
+sed -i "/export PATH=\"$INSTALL_DIR\/bin:\$PATH\"/d" "$SHELL_RC"
+
+echo "TeXbox uninstalled. Restart terminal or run 'source $SHELL_RC'"
+
